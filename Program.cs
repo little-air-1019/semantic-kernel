31d4206e9e197c7469aa1d2bd952c5f79d600580
little_air <b08409045@gmail.com>
2026-03-17 14:08:06 +0800

    init project

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.ComponentModel;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.ChatCompletion;
+using dotenv.net;
+
+internal class Program
+{
+    private static async global::System.Threading.Tasks.Task Main(string[] args)
+    {
+        // Load .env file
+        DotEnv.Load();
+
+        //Azure OpenAI 
+        var DeployName = Environment.GetEnvironmentVariable("AZURE_OPENAI_DEPLOY_NAME") ?? "👉模型佈署名稱👈";
+        var Endpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT") ?? "https://👉API端點👈.openai.azure.com/";
+        var ApiKey = Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY") ?? "👉ApiKey👈";
+
+        // Create a new kernel builder
+        var builder = Kernel.CreateBuilder()
+                    .AddAzureOpenAIChatCompletion(DeployName, Endpoint, ApiKey);
+        builder.Plugins.AddFromType<LightPlugin>(); // Add the LightPlugin to the kernel
+        Kernel kernel = builder.Build();
+
+        // Create chat history 物件，並且加入
+        var history = new ChatHistory();
+        history.AddSystemMessage("你是一個親切的智能家庭助理，可以協助用戶回答問題，並且幫我管理家電，交談時請使用中文。當我回家時，請主動幫我開燈，當我出門時，請記得幫我關燈。");
+
+        // Get chat completion service
+        var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+        // 開始對談
+        Console.Write("User > ");
+        string? userInput;
+        while (!string.IsNullOrEmpty(userInput = Console.ReadLine()))
+        {
+            // Add user input
+            history.AddUserMessage(userInput);
+
+            // Enable auto function calling
+            OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+            {
+                ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+            };
+
+            // Get the response from the AI
+            var result = await chatCompletionService.GetChatMessageContentAsync(
+                history,
+                executionSettings: openAIPromptExecutionSettings,
+                kernel: kernel);
+
+            // Print the results
+            Console.WriteLine("Assistant > " + result);
+
+            // Add the message from the agent to the chat history
+            history.AddMessage(result.Role, result.Content ?? string.Empty);
+
+            // Get user input again
+            Console.Write("User > ");
+        }
+    }
+}
+
+// 
+public class LightPlugin
+{
+    public bool IsOn { get; set; } = false;
+
+    [KernelFunction]
+    [Description("取得燈的狀態")]
+    public string GetState()
+    {
+        return IsOn ? "on" : "off";
+    }
+
+    [KernelFunction]
+    [Description("改變燈的狀態")]
+    public string ChangeState(bool newState)
+    {
+        this.IsOn = newState;
+        var state = GetState();
+
+        // Print the state to the console
+        Console.WriteLine($"[Light is now {state}]");
+
+        return state;
+    }
+}
